08aaef24cde80db469f4d64d2b652399439b10a1
Pritam Kumar <Pritam8987@gmail.com>
2021-02-27 01:25:27 -0500

    Added CRUD operations

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Task = require("./task");
 const userSchema = new mongoose.Schema({
 	name: {
 		type: String,
@@ -49,6 +50,19 @@ const userSchema = new mongoose.Schema({
 		},
 	],
 });
+userSchema.virtual("tasks", {
+	ref: "Task",
+	localField: "_id",
+	foreignField: "author",
+});
+
+userSchema.methods.toJSON = function () {
+	const user = this;
+	const userObject = user.toObject();
+	delete userObject.password;
+	delete userObject.tokens;
+	return userObject;
+};
 
 userSchema.methods.generateAuthToken = async function () {
 	//methods create instances methods
@@ -79,6 +93,13 @@ userSchema.pre("save", async function (next) {
 	if (user.isModified("password")) {
 		user.password = await bcrypt.hash(user.password, 8);
 	}
+	next();
+});
+
+//Delete user tasks when user is removed
+userSchema.pre("remove", async function (next) {
+	const user = this;
+	await Task.deleteMany({ author: user._id });
 	next();
 });
 
--- src/routers/task.js
+++ src/routers/task.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const Task = require("../models/task");
+const auth = require("../middleware/auth");
 const router = new express.Router();
 
-router.post("/tasks", async (req, res) => {
-	const task = new Task(req.body);
+router.post("/tasks", auth, async (req, res) => {
+	const task = new Task({
+		...req.body,
+		author: req.user._id,
+	});
 	try {
 		await task.save();
 		res.status(201).send(task);
@@ -11,18 +15,20 @@ router.post("/tasks", async (req, res) => {
 		res.status(400).send(err);
 	}
 });
-router.get("/tasks", async (req, res) => {
+router.get("/tasks", auth, async (req, res) => {
 	try {
-		const task = await Task.find({});
-		res.send(task);
+		// const task = await Task.find({author: req.user._id});
+		await req.user.populate("tasks").execPopulate();
+
+		res.send(req.user.tasks);
 	} catch (err) {
 		res.status(500).send(err);
 	}
 });
-router.get("/tasks/:id", async (req, res) => {
+router.get("/tasks/:id", auth, async (req, res) => {
 	const _id = req.params.id;
 	try {
-		const task = await Task.findById(_id);
+		const task = await Task.findOne({ _id, author: req.user._id });
 		if (!task) {
 			return res.status(404).send();
 		}
@@ -32,7 +38,7 @@ router.get("/tasks/:id", async (req, res) => {
 	}
 });
 
-router.patch("/tasks/:id", async (req, res) => {
+router.patch("/tasks/:id", auth, async (req, res) => {
 	const updates = Object.keys(req.body);
 	const allowedUpdates = ["description", "completed"];
 	const isValidUpdate = updates.every((update) =>
@@ -42,21 +48,28 @@ router.patch("/tasks/:id", async (req, res) => {
 		return res.status(400).send({ error: "Invalid updates" });
 	}
 	try {
-		const task = await Task.findById(req.params.id);
-		updates.forEach((update) => (task[update] = req.body[update]));
-		await task.save();
+		const task = await Task.findOne({
+			_id: req.params.id,
+			author: req.user._id,
+		});
+
 		if (!task) {
 			return res.status(404).send();
 		}
+		updates.forEach((update) => (task[update] = req.body[update]));
+		await task.save();
 		res.send(task);
 	} catch (e) {
 		res.status(400).send(e);
 	}
 });
 
-router.delete("/tasks/:id", async (req, res) => {
+router.delete("/tasks/:id", auth, async (req, res) => {
 	try {
-		const task = await Task.findByIdAndDelete(req.params.id);
+		const task = await Task.findOneAndDelete({
+			_id: req.params.id,
+			author: req.user._id,
+		});
 		if (!task) {
 			return res.status(404).send();
 		}
