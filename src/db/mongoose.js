5ca20698798f4ea0b541ac30cb1aed176b2afd80
Pritam Kumar <Pritam8987@gmail.com>
2021-03-02 21:52:40 -0500

    little change in db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,21 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URL, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	useCreateIndex: true,
-});
+
+mongoose
+	.connect(process.env.MONGODB_URL, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	})
+	.then(() => {
+		console.log(
+			"MongoDB successfully connected. Database URL:",
+			process.env.MONGODB_URL,
+		);
+	})
+	.catch((error) => {
+		console.log(
+			"Error establishing MongoDB connection. Error:",
+			error.message,
+		);
+	});
